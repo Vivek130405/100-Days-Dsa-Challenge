727ae34783a0fa7a6bccc465bb4b753ea085b725
Vivek Patil <163623130+Vivek130405@users.noreply.github.com>
2026-08-11 11:51:33 +0530

    Update

---
+++ Day73.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+
+        for (int i = 0; i <= heights.length; i++) {
+
+            int current = (i == heights.length) ? 0 : heights[i];
+
+            while (!stack.isEmpty() && heights[stack.peek()] > current) {
+
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+
+                max = Math.max(max, height * width);
+            }
+
+            stack.push(i);
+        }
+
+        return max;
+    }
+}
