d420b81b79558e1876c3e81b82884506e9b218b8
Vivek Patil <163623130+Vivek130405@users.noreply.github.com>
2026-08-22 23:26:56 +0530

    Update

---
+++ Day84.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String frequencySort(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        List<Character> list = new ArrayList<>(map.keySet());
+
+        list.sort((a, b) -> map.get(b) - map.get(a));
+
+        StringBuilder ans = new StringBuilder();
+
+        for (char ch : list) {
+            ans.append(String.valueOf(ch).repeat(map.get(ch)));
+        }
+
+        return ans.toString();
+    }
+}
