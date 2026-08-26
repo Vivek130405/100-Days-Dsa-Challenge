d50304a1cab2852ab54f8dd8cfd0f24a88d87085
Vivek Patil <163623130+Vivek130405@users.noreply.github.com>
2026-08-26 23:42:56 +0530

    Update

---
+++ Day88.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int heightChecker(int[] heights) {
+
+        int[] arr = heights.clone();
+
+        Arrays.sort(arr);
+
+        int count = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+
+            if (heights[i] != arr[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
